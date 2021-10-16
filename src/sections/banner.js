a918e4f7dca876b030e2815328f7612d6a102225
randaltk <randal.t.krebs@gmail.com>
2021-10-15 21:10:58 -0300

    atttt

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -11,7 +11,7 @@ export default function Banner() {
       <Container sx={styles.banner.container}>
         <Box sx={styles.banner.contentBox}>
           <Heading as="h1" variant="heroPrimary">
-            Top Quality Digital Products to Explore
+            Top Quality Digital Products to Exploreee
           </Heading>
         </Box>
       </Container>
